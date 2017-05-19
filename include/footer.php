1b93b804c66998dbc0ea375c74d759ccd9ffea85
mathias <mathiasqvist@hotmail.com>
2017-05-19 09:37:18 +0200

    Side-nav og Footer

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer1">
+    <div class="footer">
+        <p><span>Aarhus Street Food</span> <span> Mad Mad Mad Mad</span> <span> Tlf: 12 34 56 78 - Email: aarhusstreetfood@gmail.com</span> </p>
+    </div>
+</footer>
--- js/menu.js
+++ js/menu.js
@@ -9,12 +9,17 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 } 
 */
-
 function toggleNav() {
     var width = document.getElementById("mySidenav").style.width;
     if (width == 0) {
-        document.getElementById("mySidenav").style.width = "50vw";
-    } else {
+        document.getElementById("mySidenav").style.width = "15vw";
+    }
+    else {
         document.getElementById("mySidenav").style.width = "0";
     }
 }
+
+
+$( ".openbutton" ).click(function() {
+  $( this ).toggleClass( "closebutton" );
+});
